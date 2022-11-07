17273121c0db980531c8fe129a49d361dfe81bd3
Salvato Luice <salvatoluice5@gmai.com>
2022-11-07 20:25:21 +0300

    add inspirations

---
+++ frontend/src/components/inspirations/Inspiration.js
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+import { Link } from 'react-router-dom'
+
+const inspirationAPI = "http://localhost:9292/inspirations";
+
+
+const Inspiration = () => {
+
+    const {title, body, authors_id} = quote;
+    
+
+    function handleDelete(e) {
+        e.preventDefault();
+        fetch(`${inspirationAPI}/${quote.id}`, {
+          method: "DELETE",
+        });
+        removeQuote(quote);
+      }
+
+
+  return (
+    <div className='inspiration_quote'>
+        <h3>{title}</h3>
+        <p>{body}</p>
+        <h4>Done by: {authors_id}</h4>
+        <button>
+            <Link to={`/edit/${quote.id}`}>Edit</Link> 
+        </button>
+        <button onClick={handleDelete}>Delete</button>
+        <button onClick={() => addToFavorites(quote)}>{quote.isFav ? "Remove from Favorites" : "Add to Favorites"}</button>
+    </div>
+  )
+}
+
+export default Inspiration
