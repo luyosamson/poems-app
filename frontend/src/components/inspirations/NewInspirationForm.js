0d0d7974657cc5c45f45d4bb6f00924183993a04
Salvato Luice <salvatoluice5@gmai.com>
2022-11-07 21:06:10 +0300

    Add inspiration form

--- frontend/src/components/inspirations/NewInspirationForm.js
+++ frontend/src/components/inspirations/NewInspirationForm.js
@@ -1,9 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-const NewInspirationForm = () => {
+const quoteAPI = "http://localhost:9292/inspirations";
+
+function NewInspirationForm({addQuote}) {
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+  const [authors_id, setAuthor] = useState("");
+  const [categories_id, setCategory] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch(quoteAPI, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        title,
+        body,
+        authors_id,
+        categories_id,
+      }),
+    })
+      .then((r) => r.json())
+      .then((newQuote) => addQuote(newQuote));
+
+    setTitle("");
+    setBody("");
+    setAuthor("");
+    setCategory("");
+  }
+
   return (
-    <div>NewInspirationForm</div>
-  )
+    <form className="new-poem-form" onSubmit={handleSubmit} >
+      <input 
+        placeholder="Title" 
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <input 
+        placeholder="Author" 
+        value={authors_id}
+        onChange={(e) => setAuthor(e.target.value)}
+      />
+
+      <textarea 
+        placeholder="Write your masterpiece here..." 
+        rows={10} 
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+      />
+      
+      <input 
+        type="submit" 
+        value="Share your masterpiece" 
+      />
+    </form>
+  );
 }
 
-export default NewInspirationForm
+export default NewInspirationForm;
