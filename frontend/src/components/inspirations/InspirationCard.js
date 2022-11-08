76a9a6d2f8d6be787110a08d01f508b7d1395fe2
Salvato Luice <salvatoluice5@gmai.com>
2022-11-08 19:28:51 +0300

    Inspiration card

--- frontend/src/components/inspirations/InspirationCard.js
+++ frontend/src/components/inspirations/InspirationCard.js
@@ -3,69 +3,65 @@ import InspirationContainer from './InspirationContainer';
 import NewInspirationForm from './NewInspirationForm';
 
 const quoteAPI = "http://localhost:9292/inspirations";
+// id, title, content, author
 
-const InspirationCard = () => {
+function InspirationCard() {
+  const [quotes, setquotes] = useState([]);
+  const [formVisible, setFormVisible] = useState(true);
+  const [favoriteVisible, setFavoriteVisible] = useState(true);
+  const quotesToDisplay = quotes.filter((quote) => favoriteVisible || quote.isFavorite);
 
-    const [quotes, setquotes] = useState([]);
-    const [formVisible, setFormVisible] = useState(true);
-    const [favoriteVisible, setFavoriteVisible] = useState(true);
-    const quotesToDisplay = quotes.filter((quote) => favoriteVisible || quote.isFav);
+  useEffect(() => {
+    fetch(quoteAPI)
+      .then(res => res.json())
+      .then(data => setquotes(data))
+  }, []);
 
+  function addQuote(newQuote) {
+    setquotes([...quotes, newQuote]);
+  }
 
-    useEffect(() => {
-        fetch(quoteAPI)
-          .then(res => res.json())
-          .then(data => setquotes(data))
-      }, []);
+  function removeQuote(quoteToRemove) {
+    setquotes(quotes.filter(quote => quote.id !== quoteToRemove.id))
+  }
 
+  function addToFavorites(favquote) {
+    setquotes(quotes.map(quote => {
+      return quote.id === favquote.id ? {...favquote, isFavorite: !favquote.isFavorite} : quote
+      }  
+    ))
+  }
 
-      function addQuote(newQuote) {
-        setquotes([...quotes, newQuote]);
-      }
-
-      function removeQuote(quoteToRemove) {
-        setquotes(quotes.filter(quote => quote.id !== quoteToRemove.id))
-      }
-
-      function addToFavorites(favquote) {
-        setquotes(quotes.map(quote => {
-          return quote.id === favquote.id ? {...favquote, isFav: !favquote.isFav} : quote
-          }  
-        ))
-      }
-
-      function renderquoteView() {
-        if (quotesToDisplay.length === 0 && !favoriteVisible) {
-          return (<h1>You have no favorites added</h1>)
-        } else {
-          return (
-            <InspirationContainer 
-              quotes={quotesToDisplay} 
-              removeQuote={removeQuote} 
-              addToFavorites={addToFavorites}
-            />
-          )
-        }
-      }
-
-
+  function renderquoteView() {
+    if (quotesToDisplay.length === 0 && !favoriteVisible) {
+      return (<h1>You have no favorites added</h1>)
+    } else {
+      return (
+        <InspirationContainer 
+          quotes={quotesToDisplay} 
+          removeQuote={removeQuote} 
+          addToFavorites={addToFavorites}
+        />
+      )
+    }
+  }
 
   return (
     <div className="app">
-    <div className="sidebar">
-      <button 
-        onClick={() => setFormVisible(!formVisible)} >
-        Show/hide new quote form
-      </button>
-      {formVisible ? <NewInspirationForm addQuote={addQuote} /> : null}
+      <div className="sidebar">
+        <button 
+          onClick={() => setFormVisible(!formVisible)} >
+          Show/hide new quote form
+        </button>
+        {formVisible ? <NewInspirationForm addQuote={addQuote} /> : null}
 
-      <button onClick={() => setFavoriteVisible(!favoriteVisible)} >
-        Show/hide Favorite quotes
-      </button>
+        <button onClick={() => setFavoriteVisible(!favoriteVisible)} >
+          Show/hide Favorite quotes
+        </button>
+      </div>
+      {renderquoteView()}
     </div>
-    {renderquoteView()}
-  </div>
-  )
+  );
 }
 
-export default InspirationCard
+export default InspirationCard;
