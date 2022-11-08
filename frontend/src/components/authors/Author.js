f3260fcd8fb3cb640d96101461105ab200b78088
Salvato Luice <salvatoluice5@gmai.com>
2022-11-08 10:49:38 +0300

    Edit author

--- frontend/src/components/authors/Author.js
+++ frontend/src/components/authors/Author.js
@@ -15,26 +15,34 @@ function Author({author, removeAuthor, addToFavorites}) {
     removeAuthor(author);
   }
 
+  
+
+
+  
+//   const authorAPI =`http://localhost:9292//poemauthors/${authors_id}`
+  
+
   return (
-    <div className='scard'>
-        <div className="cards">
-        <div className="card" style={{width: 18 + 'rem', height: 400 + 'px'}}>
-            <img src={image} className="card-img-top" alt={name}/>
-            <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <button onClick={onDeleteClick} >
-                    Delete
-                </button>
-    
-                <button onClick={() => addToFavorites(author)}>
-                    {author.isFavorite ? "Unfavorite" : "♥ Favorite" }
-                </button> 
-            </div>
-            </div>
+<div className='scard'>
+    <div className="cards">
+    <div className="card" style={{width: 18 + 'rem', height: 400 + 'px'}}>
+        <img src={image} className="card-img-top" alt={name}/>
+        <div className="card-body">
+            <h5 className="card-title">{name}</h5>
+            {/* <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> */}
+            {/* <a href="#" class="btn btn-primary">Go somewhere</a> */}
+            <button onClick={onDeleteClick} >
+                Delete
+            </button>
+
+            <button onClick={() => addToFavorites(author)}>
+                {author.isFavorite ? "Unfavorite" : "♥ Favorite" }
+            </button> 
         </div>
         </div>
-      );
-    }
-    
-    export default Author;
-    
+    </div>
+    </div>
+  );
+}
+
+export default Author;
